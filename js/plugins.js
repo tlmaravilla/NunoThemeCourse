fe8cecb6f73dd62acc7475efda869e5d9503fd7a
Tom Maravilla <tlmaravilla@gmail.com>
2020-02-19 17:12:10 -0700

    Bouncing Down Arrow

--- js/plugins.js
+++ js/plugins.js
@@ -37,6 +37,11 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 });
 
 /*========== BOUNCING DOWN ARROW ==========*/
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
+    });
+});
 
 
 /*========== LIGHTBOX IMAGE GALLERY ==========*/
